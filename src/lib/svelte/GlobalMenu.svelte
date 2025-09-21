33c197f30bee17614ef1774e5be2a57e77a0ba81
Flo-Kayser <flokayser.fk@gmail.com>
2025-09-21 09:01:35 +0200

    added socials

--- src/lib/svelte/GlobalMenu.svelte
+++ src/lib/svelte/GlobalMenu.svelte
@@ -19,6 +19,42 @@
 		{ value: 'onlyBestSpecial', label: 'Beste Special-Version' }
 	];
 
+	function getIcon(name) {
+		switch (name) {
+			case 'instagram':
+				return `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M16 3a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5zm-4 5a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4m4.5-3.5a1 1 0 1 0 0 2a1 1 0 0 0 0-2"/></svg>`;
+			case 'youtube':
+				return `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"/></svg>`;
+			case 'discord':
+				return `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"/></svg>`;
+		}
+		return '';
+	}
+
+	const socialLinks = [
+		{
+			name: 'JulianFGU',
+			url: 'https://www.instagram.com/julianfgu/',
+			icon: 'instagram'
+		},
+		{
+			name: 'Entwickler',
+			url: 'https://www.instagram.com/flo.kysr/',
+			icon: 'instagram'
+		}
+	];
+
+	const otherLinks = [
+		{
+			url: 'https://www.youtube.com/@JulianFGU',
+			icon: 'youtube'
+		},
+		{
+			url: 'https://discord.gg/f65x7YYQVU',
+			icon: 'discord'
+		}
+	];
+
 	const BIT = {
 		all: 1,
 		noBase: 2,
@@ -46,9 +82,10 @@
 		return allCollected.filter((rid) => matchesVariant(rm[rid], variant)).length;
 	})();
 
-	$: activeHref = '/' +
-		navItems.find((item) => $page.url.pathname.includes(item.href.split('/')[0]))?.href ||
-		'Versions';
+	$: activeHref =
+		'/' +
+		(navItems.find((item) => $page.url.pathname.includes(item.href.split('/')[0]))?.href ||
+			'Versions');
 
 	$: isShowCounts = ['/Versions', '/Leagues', '/Countries'].some((p) =>
 		$page.url.pathname.startsWith(p)
@@ -105,7 +142,32 @@
 				</div>
 			</div>
 		</div>
-	</div>
+
+		<div class="fixed top-20 -left-36 flex divide-x-2 bg-black/80 p-1 py-2 transition-all duration-300 hover:left-0">
+			<div class="flex gap-2 pr-5 p-2 flex-col">
+				{#each socialLinks as link}
+					<a
+						href={link.url}
+						target="_blank"
+						class="flex items-center gap-2"
+						rel="noopener noreferrer"
+						>{@html getIcon(link.icon)}<span>{link.name}</span>
+					</a>
+				{/each}
+			</div>
+				<div class="flex gap-2 pr-5 p-2 flex-col pl-4">
+					{#each otherLinks as link}
+						<a
+							href={link.url}
+							target="_blank"
+							class="flex items-center gap-2"
+							rel="noopener noreferrer"
+							>{@html getIcon(link.icon)}</a
+						>
+					{/each}
+				</div>
+			</div>
+		</div>
 
 	<div class="flex lg:hidden flex-col w-full gap-2 relative">
 		<select
@@ -114,7 +176,7 @@
 			bind:value={activeHref}
 		>
 			{#each navItems as item}
-				<option value={'/'+item.href}>{item.label}</option>
+				<option value={'/' + item.href}>{item.label}</option>
 			{/each}
 		</select>
 		<span class="absolute right-2 top-3 text-xl pointer-events-none">☰</span>
@@ -147,5 +209,19 @@
 				</div>
 			</div>
 		{/if}
+
+		<div class="bg-black/70 fixed top-27 md:top-32 right-0">
+			<div class="flex gap-2 pr-5 p-2 flex-col">
+				{#each otherLinks as link}
+					<a
+						href={link.url}
+						target="_blank"
+						class="flex items-center gap-2"
+						rel="noopener noreferrer"
+						>{@html getIcon(link.icon)}
+					</a>
+				{/each}
+			</div>
+		</div>
 	</div>
 </div>
